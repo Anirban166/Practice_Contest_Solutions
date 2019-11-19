69fba144bddc59075e9c3ca739eeff5b2b3ae49f
Anirban Chetia <bloodraven166@gmail.com>
2019-11-19 21:17:29 +0530

    Codeforces 601 A (Div 2)

--- Codeforces601A.cpp
+++ Codeforces601A.cpp
@@ -1,3 +1,4 @@
+//Author:Anirban166 | Codeforces 601 A - Naive method (without DP) - 5 mins
 #include <iostream>
 #include <cstdlib>
 #include <bits/stdc++.h>
@@ -18,52 +19,56 @@ int main()
   int c=0;
   int a,b;
   cin>>a>>b;
-  if(a>0 && b>a && (b-a==5 || b-a==2 || b-a==1))
+  if(b-a==5 || b-a==2 || b-a==1 || a-b==5 || a-b==2 || a-b==1) //1 press
   cout<<1;
-  else if(a>0 && a<b && (b-a==5 || b-a==2 || b-a==1))
-  cout<<1;
-  else if(a==b)
+  else if(a==b) //0 press
   cout<<0;
   else 
   { 
-      int arr[1];
-      arr[0]=0;
-       
-  while((b-a)>=5 && b>a)
+      
+  if(b>a)     
+  {   int arr[1]; arr[0]=0;
+  while((b-a)>=5)
     { a=a+5;
       if((b-a)%2==0 || (b-a)==1 || (b-a)==3 || a>b)
       break;
       c=c+1; arr[0]+=c;
     }
     
-    while((b-a)>=2 && b>a)
+    while((b-a)>=2)
     { a=a+2;
       if((b-a)%1==0 || a>b)
       break;
       c=c+1; arr[0]+=c;
     }
-    if((b-a)>=1 && b>a)
+    if((b-a)==1)
    { c=c+1; arr[0]+=c; }
-    //------------------------------------------------------(a>b)
-    while(abs(b-a)>=5 && a>b) 
+   cout<<arr[0];
+  }
+    //---------------------------------------------------------
+  else
+  { int arr[1]; arr[0]=0;
+    while(abs(b-a)>=5) 
     { a=a-5;
       if(abs(b-a)%2==0 || (b-a)==1 || (b-a)==3 || a>b || a-5<0)
       break;
       c=c+1; arr[0]+=c;
     }
     
-    while(abs(b-a)>=2 && a>b) 
+    while(abs(b-a)>=2) 
     { a=a-2;
       if(abs(b-a)%1==0 || a>b || a-2<0)
       break;
       c=c+1; arr[0]+=c;
     }
-    if(abs(b-a)>=1 && a>b || a-1<0)
+    if(abs(b-a)==1 && a>b || a-1<0)
     {c=c+1; arr[0]+=c;}
     
     cout<<arr[0];
+  }
   }//last else
 
   }//test
   return 0; 
   }
+
