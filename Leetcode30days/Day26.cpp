8707a7ec14f7e75fe8479645d21466a739c3893e
Anirban Chetia <bloodraven166@gmail.com>
2020-04-26 12:59:42 +0530

    Create Day26.cpp

---
+++ Leetcode30days/Day26.cpp
@@ -0,0 +1,14 @@
+
+  int longestCommonSubsequence(string text1, string text2) {
+    int len1 = (int)text1.length(), len2 = (int)text2.length();
+    int dp[len1 + 1][len2 + 1];  // init size of 2D matrix
+    memset(dp, 0, sizeof(dp));   // init values to 0
+    for (int i = 1; i < len1 + 1; ++i) {
+      for (int j = 1; j < len2 + 1; ++j) {
+        dp[i][j] = text1[i - 1] == text2[j - 1]
+                       ? 1 + dp[i - 1][j - 1]
+                       : max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+    return dp[len1][len2];
+  }
